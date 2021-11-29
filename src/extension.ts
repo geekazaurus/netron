36792c434943941d3d35b3c42c8ab0515e2bdf93
Guy Hirschberg <guy.hirschberg@gmail.com>
2021-11-29 10:42:57 +0200

    Fix: models fail to load in webview
    Updated the index.html file adopted from netron.app with new javascript
    resources used by https://netron.app.
    Also added a default webpack.config.js file.

--- src/extension.ts
+++ src/extension.ts
@@ -4,19 +4,19 @@ import * as path from 'path';
 import * as vscode from 'vscode';
 import * as fs from 'fs';
 
+
 export function activate(context: vscode.ExtensionContext) {
-	context.subscriptions.push(
-		vscode.commands.registerCommand('netron.open', (resource: vscode.Uri) => {
-			const indexPath = vscode.Uri.file(
-				path.join(context.extensionPath, 'webview', 'index.html')
-			);
-			let html = fs.readFileSync(indexPath.fsPath, 'utf8');
-			let lol = vscode.window.activeTextEditor?.document.fileName;
-			if (resource !== undefined)
-			{
-				lol = resource.fsPath;
-			}
-			let baseName = path.basename(lol!);
+    context.subscriptions.push(
+        vscode.commands.registerCommand('netron.open', (resource: vscode.Uri) => {
+            const indexPath = vscode.Uri.file(
+                path.join(context.extensionPath, 'webview', 'index.html')
+            );
+            let html = fs.readFileSync(indexPath.fsPath, 'utf8');
+            let lol = vscode.window.activeTextEditor?.document.fileName;
+            if (resource !== undefined) {
+                lol = resource.fsPath;
+            }
+            let baseName = path.basename(lol!);
             const panel = vscode.window.createWebviewPanel(
                 'netron',
                 baseName + "[Netron]",
@@ -25,31 +25,31 @@ export function activate(context: vscode.ExtensionContext) {
                     enableScripts: true,
                     retainContextWhenHidden: true
                 }
-			);
-			let modelData = Buffer.from(fs.readFileSync(lol!, 'utf8'), "binary").toString("base64");
-			html = html.replace('%GRAPHNAME%', baseName);
-			html = html.replace('%MODEL%', modelData);
-			panel. webview.html = html;
-			panel.webview.onDidReceiveMessage(
-				message => {
-				  switch (message.command) {
-					case 'alert':
-					  vscode.window.showErrorMessage(message.text);
-					  return;
-					case 'confirm':
-						vscode.window.showInformationMessage(message.text, "Yes", "No").then(selection => panel.webview.postMessage({
-							command: "confirm",
-							value: selection === "Yes"
-						}));
-						return;
-				  }
-				},
-				undefined,
-				context.subscriptions
-			  );
-		})
-	);
+            );
+            let modelData = Buffer.from(fs.readFileSync(lol!, 'utf8'), "binary").toString("base64");
+            html = html.replace('%GRAPHNAME%', baseName);
+            html = html.replace('%MODEL%', modelData);
+            panel.webview.html = html;
+            panel.webview.onDidReceiveMessage(
+                message => {
+                    switch (message.command) {
+                        case 'alert':
+                            vscode.window.showErrorMessage(message.text);
+                            return;
+                        case 'confirm':
+                            vscode.window.showInformationMessage(message.text, "Yes", "No").then(selection => panel.webview.postMessage({
+                                command: "confirm",
+                                value: selection === "Yes"
+                            }));
+                            return;
+                    }
+                },
+                undefined,
+                context.subscriptions
+            );
+        })
+    );
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
